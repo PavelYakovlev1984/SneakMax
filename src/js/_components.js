72bee857894f9b4a73a29740e20ab47630611c92
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-01-24 19:25:38 +0300

    Ma first js)

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,4 +1,15 @@
+// const { name } = require("browser-sync");
+
 console.log('components');
-let favoriteColor;
-favoriteColor = prompt('What is your favorite color?');
-console.log('My favorite color is ' + favoriteColor);
+// let favoriteColor;
+// favoriteColor = prompt('What is your favorite color?');
+// console.log('My favorite color is ' + favoriteColor);
+
+let firstName;
+let lastName;
+let age;
+firstName = prompt('What is your name?');
+lastName = prompt('What is your last name?');
+age = prompt('What is your age?');
+
+console.log('My name is ' + firstName + ',' + ' My last name is ' + lastName + ',' + ' My age is ' + age + '. ');
