cfa2c6cdcdbfc176afda1409570325bd67ca6bca
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-01-26 19:07:26 +0300

    imb index

--- src/js/_components.js
+++ src/js/_components.js
@@ -5,11 +5,25 @@ console.log('components');
 // favoriteColor = prompt('What is your favorite color?');
 // console.log('My favorite color is ' + favoriteColor);
 
-let firstName;
-let lastName;
-let age;
-firstName = prompt('What is your name?');
-lastName = prompt('What is your last name?');
-age = prompt('What is your age?');
+// let firstName;
+// let lastName;
+// let age;
+// firstName = prompt('What is your name?');
+// lastName = prompt('What is your last name?');
+// age = prompt('What is your age?');
 
-console.log('My name is ' + firstName + ',' + ' My last name is ' + lastName + ',' + ' My age is ' + age + '. ');
+// console.log('My name is ' + firstName + ',' + ' My last name is ' + lastName + ',' + ' My age is ' + age + '. ');
+
+// let weight = prompt('Enter your weight');
+// let height = prompt('Enter your height');
+// let bodyMassIndex = weight / Math.pow(height, 2);
+// let overWeight = bodyMassIndex >= 25;
+// alert('your overWeight ' + overWeight);
+
+let weight = prompt('Enter your weight');
+let height = prompt('Enter your height');
+let indexMassBody = weight / Math.pow(height, 2);
+if (indexMassBody >= 25) {
+  console.log('Yor IBM more ' + (indexMassBody - 25));
+}else console.log('твой индекс меньше');
+
