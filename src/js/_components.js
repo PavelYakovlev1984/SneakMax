69f57801da9e82f9bd8c15c86eb66e7bfac5d733
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-01-27 07:48:19 +0300

    js

--- src/js/_components.js
+++ src/js/_components.js
@@ -20,10 +20,13 @@ console.log('components');
 // let overWeight = bodyMassIndex >= 25;
 // alert('your overWeight ' + overWeight);
 
-let weight = prompt('Enter your weight');
-let height = prompt('Enter your height');
-let indexMassBody = weight / Math.pow(height, 2);
-if (indexMassBody >= 25) {
-  console.log('Yor IBM more ' + (indexMassBody - 25));
-}else console.log('твой индекс меньше');
+// let weight = prompt('Enter your weight');
+// let height = prompt('Enter your height');
+// let indexMassBody = weight / Math.pow(height, 2);
+// if (indexMassBody >= 25) {
+//   console.log('Yor IBM more ' + (indexMassBody - 25));
+// }else console.log('твой индекс меньше');
+
+let age = prompt('Enter your age');
+age >= 18 ? console.log('you can bay the alcohole')+ alert('you can not bay the alcohole'): console.log('you can bay the alcohole, you are too young')+ alert('you can not bay the alcohole, you are too young')
 
