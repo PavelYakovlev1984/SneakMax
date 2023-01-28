3fdedad20971618673f3e12067849fac0c63adcf
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-01-28 14:32:38 +0300

    lesson js 39

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
+"use strict";
 // const { name } = require("browser-sync");
 
 console.log('components');
@@ -29,4 +30,41 @@ console.log('components');
 
 // let age = prompt('Enter your age');
 // age >= 18 ? console.log('you can bay the alcohole')+ alert('you can not bay the alcohole'): console.log('you can bay the alcohole, you are too young')+ alert('you can not bay the alcohole, you are too young')
+
+
+let getAverage = (sale1, sale2, sale3) => (sale1 + sale2 + sale3)/3;
+
+let dept1AverageQuarter1 = getAverage(35467, 29842, 38501);
+console.log(dept1AverageQuarter1);
+let dept2AverageQuarter1 = getAverage(70533, 500121, 33899);
+console.log(dept2AverageQuarter1);
+
+let dept1AverageQuarter2 = getAverage(50301, 21984, 19207);
+console.log(dept1AverageQuarter2);
+let dept2AverageQuarter2 = getAverage(72381, 41562, 29465);
+console.log(dept2AverageQuarter2);
+
+let printBonus = (dept1AverageQuarter1, dept2AverageQuarter1) => {
+
+  if (dept1AverageQuarter1 > dept2AverageQuarter1) {
+    let diff = dept1AverageQuarter1 - dept2AverageQuarter1;
+    let percent = (diff / dept2AverageQuarter1) * 100;
+    if(percent => 30){
+      console.log(`Dept 1 will get a bonus of ${percent}%`)
+    } else {
+      console.log('Dept 1 will not get a bonus in this quarter');
+    }
+  }
+  else if (dept1AverageQuarter1< dept2AverageQuarter1) {
+    let diff = dept2AverageQuarter1 - dept1AverageQuarter1;
+    let percent = (diff / dept2AverageQuarter1) * 100;
+    if(percent => 30){
+      console.log(`Dept 1 will get a bonus of ${percent}%`)
+    } else {
+      console.log('Dept 1 will not get a bonus in this quarter');
+    }
+  }
+}
+printBonus(dept1AverageQuarter1, dept2AverageQuarter1);
+
 
